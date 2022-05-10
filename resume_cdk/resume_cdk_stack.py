c6997759bd5fd40324140365bc381bd969baa1af
Jordan Banasik <woovie@woovie.net>
2022-05-10 12:59:08 -0700

    Fix props calls, remove trailing commas
    Trailing commas bad
    
    Forgot to include environment subsection calls

--- resume_cdk/resume_cdk_stack.py
+++ resume_cdk/resume_cdk_stack.py
@@ -28,29 +28,31 @@ class ResumeCdkStack(Stack):
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        print(props["environments"][getenv("CDK_STAGE")])
+
         site = ResumeConstructor(
             f"{props['namespace']}-construct",
+            construct_id=construct_id,
             domain_details=DomainDetails(
-                zone_id=props[getenv("CDK_STAGE")]["hosted_zone_id"],
-                zone_name=props[getenv("CDK_STAGE")]["hosted_zone_name"],
-                site_domain_name=props[getenv("CDK_STAGE")]["domain_name"],
-                custom_header_parameter_name=props[getenv("CDK_STAGE")]["custom_header_parameter_name"]
-            ),
-
+                zone_id=props["environments"][getenv("CDK_STAGE")]["hosted_zone_id"],
+                zone_name=props["environments"][getenv("CDK_STAGE")]["hosted_zone_name"],
+                site_domain_name=props["environments"][getenv("CDK_STAGE")]["domain_name"],
+                custom_header_parameter_name=props["environments"][getenv("CDK_STAGE")]["custom_header_parameter_name"]
+            )
         )
 
         CfnOutput(
             self,
             "SiteBucketName",
-            value=site.bucket.bucket_name,
+            value=site.bucket.bucket_name
         )
         CfnOutput(
             self,
             "DistributionId",
-            value=site.distribution.distribution_id,
+            value=site.distribution.distribution_id
         )
         CfnOutput(
             self,
             "CertificateArn",
-            value=site.certificate.certificate_arn,
+            value=site.certificate.certificate_arn
         )
